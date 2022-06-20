f3ab611c3faa8ee2fbdd77272ce0799e14b7f075
sharlxl <sharleneee@gmail.com>
2022-06-20 11:57:59 +0800

    init vite react ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p> hello World</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Html.tsx
@@ -0,0 +1,5 @@
+const Html = () => {
+  return <div></div>;
+};
+
+export default Html;
